efeb25d7aeb5540b5d334ae3352d3ab956ebacb5
José Javier Serrano Solís <javierserranosolis@rckgames.com>
2023-06-28 16:07:24 -0600

    Main Commit
    Uploading main project

---
+++ Assets/RCKGamesAppTemplate/Scripts/AppCore/Core/Helpers/Enum.cs
@@ -0,0 +1,33 @@
+public enum ViewID
+{
+    None = 0,
+    //Demo ViewIDs
+    PopUpViewModel,
+    ErrorViewModel,
+    SplashDemoViewModel,
+    LogInDemoViewModel,
+    MainDemoViewModel,
+    HeaderDemoViewModel,
+    SettingsDemoViewModel,
+    //App ViewIDs
+}
+
+public enum HTTPMethods
+{
+    Post,
+    Get,
+    Put,
+    Delete
+}
+
+public enum PopUpViewModelTypes
+{
+    MessageOnly,
+    OptionChoice
+}
+
+public enum MultipleCallExample
+{
+    FirstGet = 0,
+    SecondGet
+}
