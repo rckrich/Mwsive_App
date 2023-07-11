9462d010041179854f43332ed2d9a37ebe624beb
José Javier Serrano Solís <javierserranosolis@rckgames.com>
2023-07-11 09:41:44 -0600

    Checking test on get user playlist

--- Assets/Scripts/ViewModels/SpotifyConnectionDemo/SpotifyGetUserPlaylistsViewModel.cs
+++ Assets/Scripts/ViewModels/SpotifyConnectionDemo/SpotifyGetUserPlaylistsViewModel.cs
@@ -23,10 +23,17 @@ public class SpotifyGetUserPlaylistsViewModel : MonoBehaviour
 
         PlaylistRoot playlistRoot = (PlaylistRoot)_value[1];
 
-        foreach (Item item in playlistRoot.items)
+        /*foreach (Item item in playlistRoot.items)
         {
             SpotifyConnectionDemoPlaylistsHolder instance = GameObject.Instantiate(playlistHolderPrefab, instanceParent).GetComponent<SpotifyConnectionDemoPlaylistsHolder>();
             instance.Initialize(item.name, item.images[0].url);
+        }*/
+
+        for(int i = 0; i < playlistRoot.items.Count; i++)
+        {
+            Debug.Log("Current item: " + i + ", Name: " + playlistRoot.items[0].name + ", Image url: " + playlistRoot.items[i].images[0].url);
+            SpotifyConnectionDemoPlaylistsHolder instance = GameObject.Instantiate(playlistHolderPrefab, instanceParent).GetComponent<SpotifyConnectionDemoPlaylistsHolder>();
+            instance.Initialize(playlistRoot.items[i].name, playlistRoot.items[i].images[0].url);
         }
     }
 
